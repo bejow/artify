e572a99f262585e674e33ff62f06fbbb3e30e8fe
Bejowe <fast.pfeil@googlemail.com>
2018-11-28 20:45:42 +0100

    implemented a simple profile api fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import queryString from 'query-string';
 //test
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      serverData:{},
+      access_token:null,
+      refresh_token:null,
+    }
+  }
+
+  componentDidMount(){
+    let parsed = queryString.parse(window.location.search);
+    let access_token = parsed.access_token;
+    let refresh_token = parsed.refresh_token;
+    
+    const options = {
+      headers: { 'Authorization': 'Bearer ' + access_token },
+      json: true
+    }
+
+    fetch('https://api.spotify.com/v1/me', options)
+    .then((response) => {
+      response.json();
+    })
+    .then((data) => {
+      this.setState({
+        serverData:data,
+        access_token,
+        refresh_token,
+      })
+    })
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <header className="App-header">
