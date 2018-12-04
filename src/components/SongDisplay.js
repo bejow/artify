6769238cddb1ef220a48785d90339913eaa4e863
Bejowe <fast.pfeil@googlemail.com>
2018-12-04 16:33:39 +0100

    made smaller code for empty http responses handling

--- src/components/SongDisplay.js
+++ src/components/SongDisplay.js
@@ -2,19 +2,11 @@ import React from 'react';
 
 export const SongDisplay = (props) => {
     
-    if(props.song){
+
         return(
-        <div onClick={() => {props.onClick(props.song.id)}} style={{marginBottom:10, textAlign:'center', backgroundColor:'#464D59'}}>
-            <h3>{props.song.name}</h3>
-            <p>{props.song.artists[0].name}</p>
+        <div onClick={props.onClick ? () => {props.onClick(props.song.id)} : () => {console.log("No on click method")}} style={{marginBottom:10, textAlign:'center', backgroundColor:'#464D59'}}>
+            <h3>{props.song ? props.song.name : "Cannot be loaded"}</h3>
+            <p>{props.song ? props.song.artists[0].name : "empty"}</p>
         </div>
         )
-    }
-    else{
-        return (
-            <div style={{marginBottom:10, textAlign:'center', backgroundColor:'#464D59'}}>
-            <h3>Cannot be loaded</h3>
-            </div>
-        )
-    }
 }
