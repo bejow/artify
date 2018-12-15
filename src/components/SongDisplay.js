ac1f2fad40da962e36675b3be17379d783a8b622
Bejowe <fast.pfeil@googlemail.com>
2018-12-15 16:54:31 +0100

    song title position and appearance

--- src/components/SongDisplay.js
+++ src/components/SongDisplay.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export const SongDisplay = (props) => {
     return(
-        <div onClick={props.onClick ? () => {props.onClick(props.song.id)} : () => {console.log("No on click method")}} style={{textAlign:'center', position:"fixed", bottom:0, width:"100%" }}>
-            <h3>{props.song ? props.song.name : "Cannot be loaded"}</h3>
-            <p>{props.song ? props.song.artists[0].name : "empty"}</p>
+        <div onClick={props.onClick ? () => {props.onClick(props.song.id)} : () => {console.log("No on click method")}} style={{textAlign:'center', position:"fixed", bottom:5, width:"100%" }}>
+            <h3 className="songDisplayText">{props.song ? props.song.name : "Cannot be loaded"}</h3>
+            <p className="songDisplayText">{props.song ? props.song.artists[0].name : "empty"}</p>
         </div>
     )
 }
--- src/views/MainView.js
+++ src/views/MainView.js
@@ -60,7 +60,7 @@ export default class MainView extends React.Component{
 
                 <div className="new-wrapper">
                     <div id="main">
-                        <div ref="canvas" id="canvas" style={{height:"98vh"}} >
+                        <div ref="canvas" id="canvas" style={{height:"99vh"}} >
                             <P5Wrapper
                                 optionVal= {this.state.option1+this.state.option2+this.state.option3+this.state.option4}
                                 sketch={sketchAlgorithm}
